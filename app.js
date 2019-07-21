fc1bee1824c54aefed429b5ffcc4478cf9ef7299
Muhammed Baki  Almacı <bakialmaci@hacettepe.edu.tr>
2019-07-22 00:04:20 +0300

    Answer Prediction Added. Bugs has fixed.

--- app.js
+++ app.js
@@ -4,40 +4,37 @@ const bodyParser = require('body-parser');
 const utf8 = require('utf8');
 var fs = require("fs");
 var tempList = [];
-
+var newWord;
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 function getWord(){
   var array = fs.readFileSync('WordList.txt', 'utf8').toString().split("\n");
-  // console.log(array);
   where = Math.floor(Math.random() * array.length);
-  // console.log(where); 
-  var word = array[where].split("->");
+  var word = array[where].split("-");
   console.log(word);
   tempList.push(word);
   return word;
 }
 
-
 app.get('/', function (req, res) {
   getWord();
-  var newWord = tempList[tempList.length - 1];
+  newWord = tempList[tempList.length - 1];
   res.render("index",{word: newWord[0], result: null});
   console.log(tempList[tempList.length - 1]);
 })
 
 app.post('/', function (req, res) {
   if(req.body.pass){
+    console.log("asdgfh");
     getWord();
     res.redirect('back');
   }else if(req.body.answer){
-    res.render("index",{word: tempList[tempList.length - 1][0], result: tempList[tempList.length - 1][1]});
+    res.render("index",{word: newWord[0], result: newWord[1]});
     res.end();
   }
   var answer = req.body.cevap;
-  answer = answer.replace(/\s/g, '')
   tempList[tempList.length - 1][1] = tempList[tempList.length - 1][1].replace(/\s/g, '')
   console.log(answer,"-",tempList[tempList.length - 1][1]);
   console.log(answer.length,tempList[tempList.length - 1][1].length);
---
+++ server.js
@@ -0,0 +1,77 @@
+const host = "127.0.0.1";
+const port = "3000";
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+
+var fs = require("fs");
+var array = [];
+var tempList = [];
+var newWord;
+var trueCount = 0;
+var falseCount = 0;
+
+function getWord(){
+    array = fs.readFileSync('WordList.txt').toString().split("\n");
+    where = Math.floor(Math.random() * array.length);
+    newWord = array[where].split("-");
+    // console.log(newWord);
+    tempList.push(newWord);
+  }
+
+function answerVerify(answer,reply){
+    var a = answer.split("");
+    var b = reply.split("");
+    let intersection = a.filter(x => b.includes(x));
+    console.log(a.length/b.length);
+    if(a.length/b.length >= 0.8){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+function postController(req,res){
+    if(req.body.send){
+        if(answerVerify(req.body.cevap.trim(),newWord[1].trim())){
+            trueCount += 1;
+            res.redirect('/');
+            res.end();
+        }
+        else{
+            falseCount += 1;
+            console.log('True:',trueCount,'False:',falseCount);
+            res.render("index",{word: newWord[0], result: "Wrong Answer!",trues: trueCount, falses: falseCount});
+            res.end();
+        }
+
+    }
+    else if(req.body.pass){
+        res.redirect('/');
+        res.end();
+      }else if(req.body.answer){
+        res.render("index",{word: newWord[0], result: newWord[1],trues: trueCount, falses: falseCount});
+        res.end();
+      }
+}
+
+app.get('/', function (req, res) {
+    getWord();
+    res.render("index",{word: newWord[0], result: null,trues: trueCount, falses: falseCount});
+    console.log('True:',trueCount,'False:',falseCount);
+    res.end();
+})
+
+app.post('/', function (req, res) {
+    postController(req,res);
+})
+
+
+
+app.listen(3000, function () {
+    console.log('WordListApp: '+host+':'+port);
+})
